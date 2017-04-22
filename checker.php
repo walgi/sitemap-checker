937b6e904496226fe48988967b363f965bdcaa7d
maciej.kosinski@picodi.com <maciej.kosinski@picodi.com>
2017-04-22 19:51:41 +0200

    simple checker

---
+++ checker.php
@@ -0,0 +1,36 @@
+<?php
+
+	$filePath = getcwd() . '/sitemap.xml';
+	if(!file_exists($filePath)){
+		echo 'File doesnt exist';
+		exit();
+	}
+		
+	$reader = new XMLReader();
+
+	$reader->open($filePath);
+	$i = 0;
+
+	while($reader->read() && $i < 10){
+		if ($reader->nodeType == XMLReader::ELEMENT && $reader->name == 'url') {
+			$node = new \SimpleXMLElement($reader->readOuterXML());
+			$url = (string) $node->loc; 
+			$ch = curl_init($url); 
+			curl_setopt($ch, CURLOPT_HEADER, true);    // we want headers
+			curl_setopt($ch, CURLOPT_NOBODY, true);    // we don't need body
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER,1);
+			curl_setopt($ch, CURLOPT_TIMEOUT,10);
+			$output = curl_exec($ch);
+			$httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			curl_close($ch);
+	
+			if($httpcode != 200){
+				echo 'INVALID HTTP CODE [' . $httpcode . '] ' . $url . "\n";
+			}
+			$i++;			
+		}
+	}
+
+	echo 'Done!';
+
+
